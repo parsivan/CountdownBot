3c3855d5219f035a6047c8686cb7cb8bc517110a
Parsa Mahmoodi <parsa.mahmoodi81@gmail.com>
2025-12-02 18:08:49 +0400

    Dev (#1)
    * calling users name after starting bot
    
    * fixed typo | added some libraries i need probably later on
    
    * figuring out buttons | these are inline buttons
    
    * fixed a few problems pep8 found
    
    * added help command | now i understand difference between keyboard and /commands
    
    * fixed the function help with imported variables
    
    * fixed some formatting | added docstring to help_handler
    
    * changed start func from context.bot.send_message to update.message.reply_text
    
    * adding dtm to the program | echo_time now prints current utc+00:00 time | the printed time is a just a simple str no formatting
    
    * updating todo list and adding new things
    
    * fixing typo
    
    * added link to library for datetime because it is used in project
    
    * adding to todo
    
    * moving all files to root of project | deleting requirements.txt to use uv manager
    
    * plan to use uv project manager for all my python projects
    
    * added author and license
    
    * fixed typo raising syntax issue
    
    * updated by uv
    
    * for creating badges and pictures used in README files
    
    * added ruff and pytest as dev tools for project
    
    * changed order
    
    * adding badge that links to the bot
    
    * trying to make the badge work
    
    * trying to make the badge show
    
    * trying to make the badge show
    
    * trying to make the badge show
    
    * trying to make the badge show
    
    * badge works now | changed place to be under heading
    
    * final edit to todo

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import logging
+# import asyncio
+# import contextlib
+
+import datetime as dtm  # import timezone, date, datetime, time, timedelta
+from dotenv import load_dotenv
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+
+load_dotenv()  # Loading .env file
+API_KEY = os.getenv("API_KEY")  # Extracting the API_KEY from .env file
+
+
+# To get INFO about the program while running
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Respond to /start"""
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hi! How can I help you {update.message.chat.first_name}?")
+    await update.message.reply_text(
+        f"Hi! How can I help you {update.message.chat.first_name}?"
+    )
+    keyboard = [
+        [
+            InlineKeyboardButton("Get a GF", callback_data="1"),
+            InlineKeyboardButton("Get a Life", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Quit Life", callback_data="3")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+
+async def echo_time(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """print current utc time with no formatting"""
+    utc_now = dtm.datetime.now(dtm.UTC)
+    await update.message.reply_text(f"{utc_now}")  # print current utc time
+
+
+async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Reply to /help and give documentation for using bot"""
+    await update.message.reply_text(
+        "/help you can set timezone using /zone and then get your local time using /time "
+    )
+
+
+def main() -> None:
+    """Start the Bot."""
+    application = ApplicationBuilder().token(API_KEY).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("Help", help_handler))
+    application.add_handler(CommandHandler("time", echo_time))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
